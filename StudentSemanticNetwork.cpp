7f4d0c22f7704c83e082560ba3c99e68f113791e
AntonyNyarangi <antony.momanyi@gmail.com>
2016-08-18 07:36:24 +0300

    Add files via upload

---
+++ StudentSemanticNetwork.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+
+#include "SemanticNetworkClasses.h"
+
+using namespace std;
+
+// Node createStudentNode();
+// Node createCourseUnitNode();
+// Node createTeacherNode();
+// Edge createEdge(Node o_node, Node t_node);
+int main(){
+	Node created;
+	created = Graph::createNode();
+}
+// Node createStudentNode(){
+// 	Node newStudent;
+// 	string name;
+// 	int type = 1;
+
+// 	cout<<"Please enter the name of the student"<< endl;
+// 	getline(std::cin,name);
+
+// 	newStudent.setData(name,type);
+
+// 	return newStudent;
+// }
+
+// Node createCourseUnitNode(){
+// 	Node newCourse;
+// 	string name;
+// 	int type = 3;
+
+// 	cout<<"Please enter the name of the course"<< endl;
+// 	getline(std::cin,name);	
+
+// 	newCourse.setData(name, type);
+
+// 	return newCourse;
+// }
+
+// Node createTeacherNode(){
+// 	Node newTeacher;
+// 	string name;
+// 	int type = 4;
+
+// 	cout<<"Please enter the name of the Teacher"<< endl;
+// 	getline(std::cin,name);
+	
+// 	newTeacher.setData(name, type);
+
+// 	return newTeacher;
+// }
+
+// Edge createEdge(Node o_node, Node t_node){
+// 	Edge newEdge;
+// 	if(o_node.getType() == 1 && t_node.getType() == 2){
+// 		newEdge.setRelationship("Registered in");
+// 	}else if(o_node.getType() == 2 && t_node.getType() == 3){
+// 		newEdge.setRelationship("Has");
+// 	}else if(o_node.getType() == 3 && t_node.getType() == 4){
+// 		newEdge.setRelationship("Taught By");
+// 	}
+
+// 	newEdge.setOrigin(o_node);
+// 	newEdge.setTarget(t_node);	
+
+// 	return newEdge;
+// }
